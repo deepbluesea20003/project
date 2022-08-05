ca309218431919aa140bb33233e80f38e13d474e
Adam <deepbluesea20003@gmail.com>
2022-08-05 06:12:03 -0700

    A working FTP

--- Server/ftp.py
+++
@@ -1,7 +0,0 @@
-from twisted.protocols.ftp import FTPFactory, FTPRealm
-from twisted.cred.portal import Portal
-from twisted.cred.checkers import AllowAnonymousAccess, FilePasswordDB
-from twisted.internet import reactor
-
-reactor.listenTCP(21, FTPFactory(Portal(FTPRealm('./'), [AllowAnonymousAccess()])))
-reactor.run()
