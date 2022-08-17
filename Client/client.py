5a971abeb5fa352f346210a78a3d95035a5b75d2
Adam.B3 <readingfootyclub1871@gmail.com>
2022-08-17 16:19:47 +0100

    Have set up an external FTP server which can be accessed and its files downloaded. Have created basic menu system to be able to do this, which needs to be changed to taking system arguments. Files are stored in the files folder within client, the next step is to integrate the validation features.

--- Client/client.py
+++ Client/client.py
@@ -1,123 +1,79 @@
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
+# Import Module
+import ftplib
+import os
+from duplicate_check import duplicate
+
+def validateFile(file):
+    # returns True if valid, False if not
+    # needs to be linked to existing validation
+    return True
 
 
-"""
-An example of using the FTP client
-"""
-from __future__ import print_function
+def downloadFiles():
+    # Our info for our FTP server
+    HOSTNAME = "ftpupload.net"
+    USERNAME = "epiz_32401466"
+    PASSWORD = "C4tVX1tmoAf8"
 
-# Twisted imports
-from twisted.protocols.ftp import FTPClient, FTPFileListProtocol
-from twisted.internet.protocol import Protocol, ClientCreator
-from twisted.python import usage
-from twisted.internet import reactor
+    # Connect to FTP Server
+    ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
 
-# Standard library imports
-import string
-import sys
+    # force UTF-8 encoding
+    ftp_server.encoding = "utf-8"
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
+    # move to where we store our files
+    ftp_server.cwd("htdocs")
 
+    # Get list of files
+    myFiles = ftp_server.nlst()
 
-class BufferingProtocol(Protocol):
-    """Simple utility class that holds all data written to it in a buffer."""
+    # store files in the correct folder
+    os.chdir("files")
 
-    def __init__(self):
-        self.buffer = StringIO()
+    for filename in myFiles:
+        print("File is: ", filename, "\n")
+        # Enter File Name with Extension
+        # filename = "index2.html"
 
-    def dataReceived(self, data):
-        self.buffer.write(data.decode("utf-8"))
+        # TEMP
+        if filename[0] == "M":
+            print("STORING")
+            # Write file in binary mode
+            with open(filename, "wb") as file:
+                # Command for Downloading the file "RETR filename"
+                ftp_server.retrbinary(f"RETR {filename}", file.write)
+                file.close()
 
+    # Display the content of downloaded file
+    # file = open(filename, "r")
+    # print('File Content:', file.read())
 
-# Define some callbacks
-
-def success(response):
-    print('Success!  Got response:')
-    print('---')
-    if response is None:
-        print(None)
-    else:
-        print('\n'.join(response))
-    print('---')
-
-
-def fail(error):
-    print('Failed.  Error was:')
-    print(error)
+    # Close the Connection
+    ftp_server.quit()
 
 
-def showFiles(result, fileListProtocol):
-    print('Processed file listing:')
-    for file in fileListProtocol.files:
-        print('    %s: %d bytes, %s' \
-              % (file['filename'], file['size'], file['date']))
-        print(dir(file),"\n")
-    print('Total: %d files' % (len(fileListProtocol.files)))
-
-
-
-def showBuffer(result, bufferProtocol):
-    print('Got data:')
-    print(bufferProtocol.buffer.getvalue())
-
-
-class Options(usage.Options):
-    optParameters = [['host', 'h', 'localhost'],
-                     ['port', 'p', 21],
-                     ['username', 'u', 'anonymous'],
-                     ['password', None, 'twisted@'],
-                     ['passive', None, 0],
-                     ['debug', 'd', 1],
-                     ]
-
-
+# this needs to be converted t argparse, is only a temporary measure
 def run():
-    # Get config
-    config = Options()
-    config.parseOptions()
-    config.opts['port'] = int(config.opts['port'])
-    config.opts['passive'] = int(config.opts['passive'])
-    config.opts['debug'] = int(config.opts['debug'])
-
-    # Create the client
-    FTPClient.debug = config.opts['debug']
-    creator = ClientCreator(reactor, FTPClient, config.opts['username'],
-                            config.opts['password'], passive=config.opts['passive'])
-    creator.connectTCP(config.opts['host'], config.opts['port']).addCallback(connectionMade).addErrback(
-        connectionFailed)
-    reactor.run()
-
-
-def connectionFailed(f):
-    print("Connection Failed:", f)
-    reactor.stop()
-
-
-def connectionMade(ftpClient):
-    # Get the current working directory
-    ftpClient.pwd().addCallbacks(success, fail)
-
-    # Get a detailed listing of the current directory
-    fileList = FTPFileListProtocol()
-    d = ftpClient.list('.', fileList)
-    d.addCallbacks(showFiles, fail, callbackArgs=(fileList,))
-
-    # Change to the parent directory
-    ftpClient.cdup().addCallbacks(success, fail)
-
-    # Create a buffer
-    proto = BufferingProtocol()
-
-    # Get short listing of current directory, and quit when done
-    d = ftpClient.nlst('.', proto)
-    d.addCallbacks(showBuffer, fail, callbackArgs=(proto,))
-    d.addCallback(lambda result: reactor.stop())
+    try:
+        option = int(
+            input("Choose your options:\n1 : Download files\n2 : Schedule a download\n3 : Exit program\nInput : "))
+        if not isinstance(option, int):
+            print("Please enter a valid option")
+        elif option == 1:
+            print("DOWNLOAD")
+            downloadFiles()
+            print("Files up to date\n")
+        elif option == 2:
+            print("Scheduling")
+        elif option == 3:
+            # Stops the recursion
+            return 0
+        # call again if got this far
+        run()
+    except:
+        print("Please enter a valid option")
+        run()
 
 
-# this only runs if the module was *not* imported
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
