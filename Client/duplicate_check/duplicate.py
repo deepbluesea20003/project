c5af51875c21c417757affb2d5695c4bcdb6ef64
Adam.B3 <readingfootyclub1871@gmail.com>
2022-08-17 17:30:33 +0100

    checks if any batch_ids are repeated, earlier function not working

--- Client/duplicate_check/duplicate.py
+++ Client/duplicate_check/duplicate.py
@@ -1,41 +1,17 @@
 # Check for duplicate batch ids
-# Prab
+# adam
 
-import csv
-import datetime
+# Sees if there are any duplicated IDs with a csv file
 
+def checkUniqueBatchIDs(fileName):
+    # lovely bit of formatting
+    file = open(fileName, "r")
+    lines = [i.replace("\n", "").split(",") for i in file.readlines()]
 
-# Function compares two files, returns True if duplicate found, False if not
-def check_dup(file1, file2):
-    f = open(file1)
-    g = open(file2)
-    file1csv = csv.reader(f)
-    file2csv = csv.reader(g)
-    file1rows = []
-    file2rows = []
-    for entry in file1csv:
-        file1rows.append(entry[0])
-    for entry in file2csv:
-        file2rows.append(entry[0])
-    del file1rows[0]
-    del file1rows[1]
-    result = [v for v in file1rows if v in file2rows]
+    # read in all batch ids
+    ids = [i[0] for i in lines]
 
-    f.close()
-    g.close()
-
-    if result:
-        return True
-    else:
-        return False
+    # returns true if all ids are unique, false if not
+    return len(ids) == len(set(ids))
 
 
-# Example
-if check_dup('MED_DATA_20220803153918.csv', 'MED_DATA_20220803153920.csv'):
-    print("DUPLICATE BATCH IDs FOUND")
-    f = open('log.txt', 'a+')
-    error_msg = str(
-        datetime.datetime.now()) + ': ERROR DUPLICATE:MED_DATA_20220803153918.CSV AND MED_DATA_20220803153920.CSV ' \
-                                   'HAVE DUPLICATE BATCH IDs\n '
-    f.write(error_msg)
-    f.close()
